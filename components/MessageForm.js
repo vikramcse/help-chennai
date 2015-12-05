fc0c59042f670e24e82e2b71278f88b33e407553
vikramcse <vikramcse.10@gmail.com>
2015-12-05 19:44:02 +0530

    Message Form

---
+++ components/MessageForm.js
@@ -0,0 +1,35 @@
+var React = require('react');
+
+var MessageForm = React.createClass({
+	getInitialState() {
+		return {
+			value: ''
+		};
+	},
+	handleChange() {
+		this.setState({
+			value: event.target.value
+		});
+	},
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props.onSearch(this.state.value);
+	},
+	render() {
+		return (
+			<div className="message-holder">
+				<form id="geocoding_form" className="form-horizontal" onSubmit={this.handleSubmit}>
+					<div className="form-group">
+						<div className="area">
+							<label>Message</label>
+							<textarea value={this.state.value} onChange={this.handleChange} className="form-control" id="message" placeholder="Enter Message"/>
+							<button className="btn btn-primary btn-block">Submit Message</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		);
+	}
+});
+
+module.exports = MessageForm;
